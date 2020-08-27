6bed9d03defda24c0b9eab048d6e5ee44646412a
Adao Oliveira <adaum79@gmail.com>
2020-08-27 14:48:20 -0300

    Concluido capitulo #14

--- app/api/decorators.py
+++ app/api/decorators.py
@@ -1,7 +1,6 @@
 from functools import wraps
-from flask import abort
-from flask_login import current_user
-from .models import Permission
+from flask import g
+from .errors import forbidden
 
 
 def permission_required(permission):
@@ -9,11 +8,7 @@ def permission_required(permission):
         @wraps(f)
         def decorated_function(*args, **kwargs):
             if not g.current_user.can(permission):
-                abort(403)
+                return forbidden('Insufficient permissions')
             return f(*args, **kwargs)
         return decorated_function
     return decorator
-
-
-def admin_required(f):
-    return permission_required(Permission.ADMIN)(f)
--- app/models.py
+++ app/models.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 import hashlib
-from flask import current_app, request
+from flask import current_app, request, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from markdown import markdown
 import bleach
 from . import db, login_manager
+from .exceptions import ValidationError
 
 
 class Permission:
@@ -90,6 +91,25 @@ class Post(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     comments = db.relationship('Comment', backref='post', lazy='dynamic')
+
+    def to_json(self):
+        json_post = {
+            'url': url_for('api.get_post', id=self.id),
+            'body': self.body,
+            'body_html': self.body_html,
+            'timestamp': self.timestamp,
+            'author_url': url_for('api.get_user', id=self.author_id),
+            'comments_url': url_for('api.get_post_comments', id=self.id),
+            'comment_count': self.comments.count()
+        }
+        return json_post
+
+    @staticmethod
+    def from_json(json_post):
+        body = json_post.get('body')
+        if body is None or body == '':
+            raise ValidationError('post does not have a body')
+        return Post(body=body)
 
     @staticmethod
     def on_changed_body(target, value, oldvalue, initiator):
@@ -283,6 +303,34 @@ class User(UserMixin, db.Model):
                 db.session.add(user)
                 db.session.commit()
 
+    def generate_auth_token(self, expiration):
+        s = Serializer(current_app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return None
+        return User.query.get(data['id'])
+
+    def to_json(self):
+        json_user = {
+            'url': url_for('api.get_user', id=self.id),
+            'username': self.username,
+            'member_since': self.member_since,
+            'last_seen': self.last_seen,
+            'posts_url': url_for('api.get_user_posts', id=self.id),
+            'followed_posts_url': url_for(
+                'api.get_user_followed_posts',
+                id=self.id
+            ),
+            'post_count': self.posts.count()
+        }
+        return json_user
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -310,6 +358,24 @@ class Comment(db.Model):
     disabled = db.Column(db.Boolean)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+
+    def to_json(self):
+        json_comment = {
+            'url': url_for('api.get_comment', id=self.id),
+            'post_url': url_for('api.get_post', id=self.post_id),
+            'body': self.body,
+            'body_html': self.body_html,
+            'timestamp': self.timestamp,
+            'author_url': url_for('api.get_user', id=self.author_id),
+        }
+        return json_comment
+
+    @staticmethod
+    def from_json(json_comment):
+        body = json_comment.get('body')
+        if body is None or body == '':
+            raise ValidationError('comment does not have a body')
+        return Comment(body=body)
 
     @staticmethod
     def on_changed_body(target, value, oldvalue, initiator):
